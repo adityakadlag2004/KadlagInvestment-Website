7d5cfce79653954fd71040069df1853c657afe7b
Aditya Kadlag <adityakadlag2004@gmail.com>
2023-08-02 12:54:11 +0530

    CSS Contact form

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CustomerForm(models.Model):
+    name = models.CharField(max_length=550)
+    email = models.CharField(max_length=300)
+    phno = models.CharField(max_length=12)
+    services = models.CharField(max_length=600)
+    
--- Home/views.py
+++ Home/views.py
@@ -17,6 +17,8 @@ def test_page(request):
 
 
 def contact_us(request):
+    if request.POST:
+        name = request.POST['name']
     return render(request, 'contactus.html')
 
 
@@ -25,9 +27,9 @@ def about(request):
     params['div1passage'] = "Kadlag Investment is a reputable and well-established financial planning firm with over 25 years of experience in the industry. With a track record of excellence, we have successfully served more than 2000 satisfied customers. Our expertise lies in wealth management, where we have efficiently managed over 100 crores in assets. At Kadlag Investment, we understand the importance of comprehensive financial planning. In addition to our wealth management services, we offer a range of solutions to meet our clients' diverse needs. Our services include mediclaim and insurance, ensuring that our customers have access to the necessary coverage for their health and financial security.With a team of highly skilled professionals, we provide personalized and tailored solutions to our clients. We take pride in our ability to understand our clients' financial goals and develop strategies that align with their aspirations. Our commitment to delivering exceptional service, coupled with our extensive experience and expertise, sets us apart as a trusted partner in financial planning.Choose Kadlag Investment for your financial needs and experience the peace of mind that comes with working with a reliable and customer-centric firm. We are dedicated to helping individuals and businesses achieve financial success, making us the preferred choice for those seeking expert financial guidance."
 
     params['aumdes'] = "Kadlag Investment is a leading financial firm specializing in asset management. With an impressive portfolio totaling 100 crores in assets under management, we have a proven track record of successfully growing our clients' wealth.Our top priority is client satisfaction, and we take great pride in our extensive base of happy customers."
-    
+
     params['cusdes'] = "Kadlag Investment is a renowned investment firm with an impressive track record of serving over 2500 satisfied customers. With our expertise and commitment to excellence, we have become a trusted partner for individuals and businesses seeking reliable investment solutions."
-    
+
     params['yeardes'] = "Dear valued clients and partners,We are thrilled to share with you that Kadlag Investment has reached a significant milestone in our journey. For over 25 years, we have been committed to providing exceptional investment services and expertise to our esteemed clients."
-    
+
     return render(request, 'about.html', params)
